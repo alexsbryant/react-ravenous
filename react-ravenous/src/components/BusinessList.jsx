59efde94820b6ca2ba48315e883a5d8de92fe40a
Alexander Bryant <asbryant@me.com>
2024-12-19 20:19:03 -0700

    Create BusinessList.jsx

---
+++ react-ravenous/src/components/BusinessList.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class BusinessList extends Component {
+  render() {
+    return (
+      <div>BusinessList</div>
+    )
+  }
+}
+
+export default BusinessList;
