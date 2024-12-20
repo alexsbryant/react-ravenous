49a4c8a9f61affcb8c87cc0f8af748f12c4b4651
Alexander Bryant <asbryant@me.com>
2024-12-19 18:28:23 -0700

    Create business.jsx
    First steps of creating business jsx

---
+++ react-ravenous/src/components/business.jsx
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+
+function Business() {
+
+    const businesses = {
+        /* CREATE OBJECT FOR BUSINESSES */
+    }
+
+  return (
+    <div>business</div>
+  )
+}
+
+export default Business;
