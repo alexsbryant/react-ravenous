16cf2f2878f58a7edd0bd4f87cd0ebb97daaf536
Alexander Bryant <asbryant@me.com>
2025-01-02 12:24:41 -0700

    Update App.jsx
    Removed unused import

--- react-ravenous/src/App.jsx
+++ react-ravenous/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import styles from './App.module.css'
-import Business from './components/Business/Business';
 import BusinessList from './components/BusinessList/BusinessList';
 import SearchBar from './components/SearchBar/SearchBar';
 
